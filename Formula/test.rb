09cb0569e8a79de4baa380f0f8170807b796cac6
yourmoonlight <foxkingmoonlight@gmail.com>
2022-05-04 21:22:06 +0800

    test add formula

---
+++ Formula/test.rb
@@ -0,0 +1,11 @@
+class Test < Formula
+    desc "Test Homebrew Tabs"
+    homepage "https://www.starcoin.org/"
+    url "https://github.com/starcoinorg/starcoin/releases/download/v1.11.7-rc/starcoin-macos-latest.zip"
+    sha256 "3bf4b13c9df7e17a1fc22f42fca4ce93a79cb89d0598c97985c7499a4385e1c8"
+    version "v1.11.7-rc"
+
+    def install
+      bin.install "starcoin"
+    end
+  end
