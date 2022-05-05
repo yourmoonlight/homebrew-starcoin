18210c3f6551fb543b7d7847875a0d284ca59137
yourmoonlight <foxkingmoonlight@gmail.com>
2022-05-05 15:25:12 +0800

    test change version

--- Formula/starcoin.rb
+++ Formula/starcoin.rb
@@ -1,9 +1,9 @@
 class Starcoin < Formula
     desc "Test Homebrew Tabs"
     homepage "https://www.starcoin.org/"
-    url "https://github.com/starcoinorg/starcoin/releases/download/v1.11.6-alpha/starcoin-macos-latest.zip"
-    sha256 "c81a48093b62c29a1be5589ac98e1534b048ce2a115df2a123a40d2b2e8fbb76"
-    version "v1.11.6-alpha"
+    url "https://github.com/starcoinorg/starcoin/releases/download/v1.11.7-rc/starcoin-macos-latest.zip"
+    sha256 "3bf4b13c9df7e17a1fc22f42fca4ce93a79cb89d0598c97985c7499a4385e1c8"
+    version "v1.11.7-rc"
 
     def install
       bin.install "starcoin"
